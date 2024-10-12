2cf2449eecfcb3ab2dcab8cece4598db716cd208
Jhonatan Prieto <jhonatan0913@gmail.com>
2024-10-12 18:00:01 -0500

    Se crea la clase Ventas con los cuatro metodos

---
+++ FincaAgricolaWebApp/Data/VentasDat.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class VentasDat
+    {
+        // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
+        Persistence objPer = new Persistence();
+
+
+        // Método para mostrar las ventas desde la base de datos.
+        public DataSet showVentas()
+        {
+            // Se crea un adaptador de datos para MySQL.
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+
+            // Se crea un DataSet para almacenar los resultados de la consulta.
+            DataSet objData = new DataSet();
+
+            // Se crea un comando MySQL para seleccionar los productos utilizando un procedimiento almacenado.
+            MySqlCommand objSelectCmd = new MySqlCommand();
+
+            // Se establece la conexión del comando utilizando el método openConnection() de Persistence.
+            objSelectCmd.Connection = objPer.openConnection();
+
+            // Se especifica el nombre del procedimiento almacenado a ejecutar.
+            objSelectCmd.CommandText = "sp_show_ventas";
+
+            // Se indica que se trata de un procedimiento almacenado.
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se establece el comando de selección del adaptador de datos.
+            objAdapter.SelectCommand = objSelectCmd;
+
+            // Se llena el DataSet con los resultados de la consulta.
+            objAdapter.Fill(objData);
+
+            // Se cierra la conexión después de obtener los datos.
+            objPer.closeConnection();
+
+            // Se devuelve el DataSet que contiene los productos.
+            return objData;
+        }
+
+        //Metodo para guardar una nueva venta
+        public bool saveVentas(DateTime _fecha, double _monto)
+        {
+            // Se inicializa una variable para indicar si la operación se ejecutó correctamente.
+            bool executed = false;
+            int row;// Variable para almacenar el número de filas afectadas por la operación.
+
+            // Se crea un comando MySQL para insertar una nueva venta utilizando un procedimiento almacenado.
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_insert_ventas"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan parámetros al comando para pasar los valores de la venta.
+            objSelectCmd.Parameters.Add("v_vent_fecha", MySqlDbType.VarString).Value = _fecha;
+            objSelectCmd.Parameters.Add("v_vent_monto", MySqlDbType.VarString).Value = _monto;
+
+            try
+            {
+                // Se ejecuta el comando y se obtiene el número de filas afectadas.
+                row = objSelectCmd.ExecuteNonQuery();
+
+                // Si se inserta una fila correctamente, se establece executed a true.
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                // Si ocurre un error durante la ejecución del comando, se muestra en la consola.
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            // Se devuelve el valor de executed para indicar si la operación se ejecutó correctamente.
+            return executed;
+        }
+
+        //Metodo para actulizar una venta
+        public bool updateVentas(int _id, DateTime _fecha, double _monto, int clieId)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_update_ventas"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Se agregan parámetros al comando para pasar los valores del producto.
+            objSelectCmd.Parameters.Add("v_vent_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("v_vent_fecha", MySqlDbType.VarString).Value = _fecha;
+            objSelectCmd.Parameters.Add("v_vent_monto", MySqlDbType.VarString).Value = _monto;
+            objSelectCmd.Parameters.Add("v_clie_id", MySqlDbType.Int32).Value = clieId;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        //Metodo para borrar una Categoria
+        public bool deleteVentas(int _id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_delete_ventas"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("v_vent_id", MySqlDbType.Int32).Value = _idCategory;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
