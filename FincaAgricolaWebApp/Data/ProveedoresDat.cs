35cc019adc370bb9cf8b634af6f52919f8ef91ce
manuel erazo <meerazo7@hotmail.com>
2024-10-13 14:27:25 -0500

    se crea la clase proveedor con los 4 métodos

---
+++ FincaAgricolaWebApp/Data/ProveedoresDat.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class ProveedoresDat
+    {
+        // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
+        Persistence objPer = new Persistence();
+
+        public DataSet showProveedor()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_show_proveedores"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+
+            objPer.closeConnection();
+
+            return objData;
+        }
+
+        public bool saveProveedor(string _nombre, string _contacto, string _tipo)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_insert_proveedores"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agrega los parámetros correspondientes
+            objSelectCmd.Parameters.Add("v_pro_nombre", MySqlDbType.VarChar).Value = _nombre;
+            objSelectCmd.Parameters.Add("v_pro_contacto", MySqlDbType.VarChar).Value = _contacto;
+            objSelectCmd.Parameters.Add("v_pro_tipo_insumo", MySqlDbType.VarChar).Value = _tipo;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool updateProveedor(int _id, string _nombre, string _contacto, string _tipo)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_update_proveedores"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agrega los parámetros correspondientes
+            objSelectCmd.Parameters.Add("v_pro_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("v_pro_nombre", MySqlDbType.VarChar).Value = _nombre;
+            objSelectCmd.Parameters.Add("v_pro_contacto", MySqlDbType.VarChar).Value = _contacto;
+            objSelectCmd.Parameters.Add("v_pro_tipo_insumo", MySqlDbType.VarChar).Value = _tipo;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool deleteProveedor(int _id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_delete_proveedores"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agrega el parámetro correspondiente
+            objSelectCmd.Parameters.Add("v_pro_id", MySqlDbType.Int32).Value = _id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
