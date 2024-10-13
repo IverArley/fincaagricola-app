421a9397c6f9cd9014da18226fac93b7e945b149
manuel erazo <meerazo7@hotmail.com>
2024-10-13 14:18:54 -0500

    se crea la clase cultivos con los 4 métodos

--- FincaAgricolaWebApp/Data/CultivosDat.cs
+++ FincaAgricolaWebApp/Data/CultivosDat.cs
@@ -7,5 +7,119 @@ namespace Data
 {
     public class CultivosDat
     {
+        // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
+        Persistence objPer = new Persistence();
+
+        public DataSet showCultivo()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_show_empleados"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+
+            objPer.closeConnection();
+
+            return objData;
+        }
+
+        public bool saveCultivo(string _nombre, DateTime _fecha, string _estado, int _parcId)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_insert_cultivos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agrega los parámetros correspondientes
+            objSelectCmd.Parameters.Add("v_cult_nombre", MySqlDbType.VarChar).Value = _nombre;
+            objSelectCmd.Parameters.Add("v_cult_fecha_siembra", MySqlDbType.Date).Value = _fecha;
+            objSelectCmd.Parameters.Add("v_cult_estado", MySqlDbType.VarChar).Value = _estado;
+            objSelectCmd.Parameters.Add("v_parc_id", MySqlDbType.Int32).Value = _parcId;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool updateCultivo(int _id, string _nombre, DateTime _fecha, string _estado, int _parcId)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_update_empleados"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agrega los parámetros correspondientes
+            objSelectCmd.Parameters.Add("v_cult_id", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("v_cult_nombre", MySqlDbType.VarChar).Value = _nombre;
+            objSelectCmd.Parameters.Add("v_cult_fecha_siembra", MySqlDbType.Date).Value = _fecha;
+            objSelectCmd.Parameters.Add("v_cult_estado", MySqlDbType.VarChar).Value = _estado;
+            objSelectCmd.Parameters.Add("v_parc_id", MySqlDbType.Int32).Value = _parcId;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool deleteCultivo(int _id)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "sp_delete_cultivos"; // Nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+
+            // Agrega el parámetro correspondiente
+            objSelectCmd.Parameters.Add("v_cult_id", MySqlDbType.Int32).Value = _id;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
     }
 }
