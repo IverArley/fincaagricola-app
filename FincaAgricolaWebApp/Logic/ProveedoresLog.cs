66a4157c43f71d5ea574184d170ecd34f69ed31a
Iver Mendez <iverarleyb@gmail.com>
2024-10-26 20:56:17 -0500

    Clase ProveedoresLog

--- FincaAgricolaWebApp/Logic/ProveedoresLog.cs
+++ FincaAgricolaWebApp/Logic/ProveedoresLog.cs
@@ -12,131 +12,32 @@ namespace Logic
     public class ProveedoresLog
     {
         // Se crea una instancia de la clase Persistence para manejar la conexión a la base de datos.
-        ProveedoresLog objPro = new ();
+        ProveedoresLog objPro = new ProveedoresLog();
 
         public DataSet showProveedor()
         {
-            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
-            DataSet objData = new DataSet();
-
-            MySqlCommand objSelectCmd = new MySqlCommand();
-            objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "sp_show_proveedores"; // Nombre del procedimiento almacenado
-            objSelectCmd.CommandType = CommandType.StoredProcedure;
-
-            objAdapter.SelectCommand = objSelectCmd;
-            objAdapter.Fill(objData);
-
-            objPer.closeConnection();
-
-            return objData;
+            return objPro.showProveedor();
         }
+    
         //Metodo para mostrar unicamente el id y el nombre
         public DataSet showProvedoresDDL()
         {
-            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
-            DataSet objData = new DataSet();
-
-            MySqlCommand objSelectCmd = new MySqlCommand();
-            objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "sp_show_proveedores_ddl";
-            objSelectCmd.CommandType = CommandType.StoredProcedure;
-            objAdapter.SelectCommand = objSelectCmd;
-            objAdapter.Fill(objData);
-            objPer.closeConnection();
-            return objData;
+            return objPro.showProvedoresDDL();
         }
 
         public bool saveProveedor(string _nombre, string _contacto, string _tipo)
         {
-            bool executed = false;
-            int row;
-
-            MySqlCommand objSelectCmd = new MySqlCommand();
-            objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "sp_insert_proveedores"; // Nombre del procedimiento almacenado
-            objSelectCmd.CommandType = CommandType.StoredProcedure;
-
-            // Agrega los parámetros correspondientes
-            objSelectCmd.Parameters.Add("v_pro_nombre", MySqlDbType.VarChar).Value = _nombre;
-            objSelectCmd.Parameters.Add("v_pro_contacto", MySqlDbType.VarChar).Value = _contacto;
-            objSelectCmd.Parameters.Add("v_pro_tipo_insumo", MySqlDbType.VarChar).Value = _tipo;
-
-            try
-            {
-                row = objSelectCmd.ExecuteNonQuery();
-                if (row == 1)
-                {
-                    executed = true;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error " + e.ToString());
-            }
-            objPer.closeConnection();
-            return executed;
+            return objPro.saveProveedor(_nombre,  _contacto,  _tipo);
         }
 
         public bool updateProveedor(int _id, string _nombre, string _contacto, string _tipo)
         {
-            bool executed = false;
-            int row;
-
-            MySqlCommand objSelectCmd = new MySqlCommand();
-            objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "sp_update_proveedores"; // Nombre del procedimiento almacenado
-            objSelectCmd.CommandType = CommandType.StoredProcedure;
-
-            // Agrega los parámetros correspondientes
-            objSelectCmd.Parameters.Add("v_pro_id", MySqlDbType.Int32).Value = _id;
-            objSelectCmd.Parameters.Add("v_pro_nombre", MySqlDbType.VarChar).Value = _nombre;
-            objSelectCmd.Parameters.Add("v_pro_contacto", MySqlDbType.VarChar).Value = _contacto;
-            objSelectCmd.Parameters.Add("v_pro_tipo_insumo", MySqlDbType.VarChar).Value = _tipo;
-
-            try
-            {
-                row = objSelectCmd.ExecuteNonQuery();
-                if (row == 1)
-                {
-                    executed = true;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error " + e.ToString());
-            }
-            objPer.closeConnection();
-            return executed;
+            return objPro.updateProveedor(_id,_nombre, _contacto, _tipo);
         }
 
         public bool deleteProveedor(int _id)
         {
-            bool executed = false;
-            int row;
-
-            MySqlCommand objSelectCmd = new MySqlCommand();
-            objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "sp_delete_proveedores"; // Nombre del procedimiento almacenado
-            objSelectCmd.CommandType = CommandType.StoredProcedure;
-
-            // Agrega el parámetro correspondiente
-            objSelectCmd.Parameters.Add("v_pro_id", MySqlDbType.Int32).Value = _id;
-
-            try
-            {
-                row = objSelectCmd.ExecuteNonQuery();
-                if (row == 1)
-                {
-                    executed = true;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error " + e.ToString());
-            }
-            objPer.closeConnection();
-            return executed;
+            return objPro.deleteProveedor(_id);
         }
     }
 }
